f7e07dedc61dd6f6047480cc6108c0f3ec6b2bc6
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2015-03-15 13:09:14 -0400

    Modified dguide.m
    Changed the input to lambda from frequency and changed the c0 and a to
    be in m/s and m

--- Orfanidis_codes/dguide.m
+++ Orfanidis_codes/dguide.m
@@ -1,10 +1,10 @@
 % dguide.m - TE modes in dielectric slab waveguide
 %
-% Usage: [be,kc,ac,fc,err] = dguide(f,a,n1,n2,Nit)
-%        [be,kc,ac,fc,err] = dguide(f,a,n1,n2)     (equivalent to Nit=3)
+% Usage: [be,kc,ac,fc,err] = dguide(lambda,a,n1,n2,Nit)
+%        [be,kc,ac,fc,err] = dguide(lambda,a,n1,n2)     (equivalent to Nit=3)
 %
-% f     = frequency in GHz
-% a     = half-width of slab in cm
+% lambda= wavlength of the light
+% a     = half-width of slab
 % n1,n2 = refractive indices of slab and cladding (n1>n2)
 % Nit   = number of Newton iterations (default Nit=3)
 %
@@ -18,19 +18,20 @@
 
 % Sophocles J. Orfanidis - 1999-2008 - www.ece.rutgers.edu/~orfanidi/ewa
 
-function [be,kc,ac,fc,err] = dguide(f,a,n1,n2,Nit)
+function [be,kc,ac,fc,err] = dguide(lambda,a,n1,n2,Nit)
 
 if nargin==0, help dguide; return; end
 if nargin==4, Nit=3; end;
 
-c0 = 30;                                    % c0 in GHz x cm
-k0 = 2*pi*f/c0;                             % free-space wavenumber
+c0 = 3e8;                                   % c0 in m/s
+k0 = 2*pi/lambda;%= 2*pi*f/c0;              % free-space wavenumber
 NA = sqrt(n1^2 - n2^2);                     % numerical aperture 
 R = k0*a*NA;                                % circle radius
 M = floor(2*R/pi);                          % number of modes = M+1
 m = 0:M;                                    % mode numbers
 Rc = m*pi/2;                                % cutoff radii
 fc = c0*Rc/(2*pi*a*NA);                     % cutoff frequencies in GHz
+%lambdac = 2*pi/(Rc/(2*pi*a*NA));
 
 [u,v,err] = dslab(R,Nit);                   
 
